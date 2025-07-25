8c20e589cdee13d279803f86c7edd101467504c9
Snoopy <dfg2amirali@gmail.com>
2025-07-25 14:40:10 +0330

    feat: implement dark mode toggle using useEffect and localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Navbar from './Components/Navbar'
+import Footer from './Components/Footer'
 
 export default function App() {
+  const [isDark , setIsDark] = useState(false);
+
+  useEffect(() => {
+     const savedTheme = localStorage.getItem('theme');
+     if(savedTheme == 'dark') {
+      document.documentElement.classList.add('dark');
+     }
+  },[]);
+
+  const toggleTheme = () => {
+    setIsDark(!isDark);
+    if(!isDark){
+      document.documentElement.classList.add('dark');
+    }else{
+      document.documentElement.classList.remove('dark');
+    }
+  }
   return (
     <>
-      <div></div> 
+      <Navbar />
+      <Footer />
     </>
   )
 }
